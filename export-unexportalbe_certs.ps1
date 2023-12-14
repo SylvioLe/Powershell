32057cc5c5b195707de36fe63a34efa19d781af0
SylvioLe <sylviokaufmann@yahoo.de>
2023-12-14 08:17:07 +0100

    Update export-unexportalbe_certs.ps1

--- export-unexportalbe_certs.ps1
+++ export-unexportalbe_certs.ps1
@@ -1,1 +1,8 @@
-#
+# this script exports unexportable marked certificates bypassing password and export security
+# the number in the brackets indicates the target certificate
+
+$cert = (dir cert:\currentuser\root)[0]
+$type = [System.Security.Cryptography.X509Certificates.X509ContentType]::pfx
+$pass = ConvertTo-SecureString "Hurra" -AsPlainText -Force
+$bytes = $cert.export($type, $pass)
+[System.IO.File]::WriteAllBytes("f:\file.pfx", $bytes)
