6ca848dffe7f5d253473c4d7bc8e734d54eebeff
SylvioLe <sylviokaufmann@yahoo.de>
2024-09-12 14:49:14 +0200

    Update export-unexportable_certs.ps1

--- export-unexportable_certs.ps1
+++ export-unexportable_certs.ps1
@@ -1,6 +1,6 @@
 # this script exports unexportable marked certificates bypassing password and export security
 # the number in the brackets indicates the target certificate
-
+# JUST WORKS WITH ADDITIONAL ADDED NON-MICROSOFT STANDARD CERTIFICATES
 $cert = (dir cert:\currentuser\root)[0]
 $type = [System.Security.Cryptography.X509Certificates.X509ContentType]::pfx
 $pass = ConvertTo-SecureString "Hurra" -AsPlainText -Force
