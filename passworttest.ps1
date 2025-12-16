0aba65340093ed960b18f7095fbc0346861b310c
SylvioLe <sylviokaufmann@yahoo.de>
2025-12-16 09:53:46 +0100

    Add Passworttest function for credential validation

---
+++ passworttest.ps1
@@ -0,0 +1,21 @@
+function Passworttest {
+    [CmdletBinding()]
+    Param
+    (
+        [string]$User,
+        [string]$Passwort
+    )
+    if (!($User) -or !($Passwort)) {
+        Write-Warning 'Username und Passwort angeben'
+    } else {
+        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+        $DS = New-ObjectSystem.DirectoryServices.AccountManagement.PrincipalContext('domain')
+        $DS.ValidateCredentials($User, $Passwort)
+    }
+}
+
+$User = read-host "Username eingeben"
+$Passwort = read-host "Passwort eingeben"
+Passworttest $User $Passwort
+$User = " "
+$Passwort = " "
